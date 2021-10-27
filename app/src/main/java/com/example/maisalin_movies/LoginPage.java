e418deca5a7868b75c87cf75269eebf5d7f6f0fc
User <maisalin2015@gmail.com>
2021-10-27 14:29:30 +0300

    maisaleen

--- app/src/main/java/com/example/maisalin_movies/LoginPage.java
+++ app/src/main/java/com/example/maisalin_movies/LoginPage.java
@@ -1,25 +1,40 @@
 package com.example.maisalin_movies;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginPage extends AppCompatActivity implements View.OnLongClickListener {
 
+
+    private static final String TAG ="FIREBASE" ;
     //declaring all the components
     private EditText editTextName, editTextPass;
     private ImageView buttonLogin;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        //Returns a reference to the instance of the project Firebase
+        mAuth = FirebaseAuth.getInstance();
 
         // findViewById returns reference to the object with the specified id
         editTextName = findViewById(R.id.editTextName);
@@ -54,6 +69,8 @@ public class LoginPage extends AppCompatActivity implements View.OnLongClickList
             editor.commit();
 
 
+           // login(editTextName.getText().toString(), editTextPass.getText().toString());
+
             intent.putExtra("name", editTextName.getText().toString());
             startActivity(intent);
         }
@@ -67,5 +84,27 @@ public class LoginPage extends AppCompatActivity implements View.OnLongClickList
         editTextPass.setText("");
 
         return true;
+    }
+
+    public void login(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginPage.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/maisalin_movies/SignupPage.java
+++ app/src/main/java/com/example/maisalin_movies/SignupPage.java
@@ -1,14 +1,70 @@
 package com.example.maisalin_movies;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class SignupPage extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignupPage extends AppCompatActivity implements View.OnLongClickListener {
+
+
+    private static final String TAG = "FIREBASE";
+    //declaring all the components
+    private EditText editTextName, editTextPass;
+    private Button button;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_page);
+        //Returns a reference to the instance of the project Firebase
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+
+    public void submit(View view){
+        signUp(editTextName.getText().toString(),editTextPass.getText().toString());
+    }
+    public void signUp(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent i= new Intent(SignupPage.this,MainActivity.class);
+                            startActivity(i);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignupPage.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+                });
+    }
+
+    @Override
+    public boolean onLongClick(View view) {
+        return false;
     }
 }
