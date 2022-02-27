7f684cd574336017764c436ae591be87a487b540
User <maisalin2015@gmail.com>
2022-02-27 20:39:54 +0200

    maisaleen

--- app/src/main/java/com/example/maisalin_movies/MainActivity.java
+++ app/src/main/java/com/example/maisalin_movies/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.maisalin_movies;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
 
 import android.app.AlarmManager;
 import android.app.AlertDialog;
@@ -10,15 +12,18 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.RelativeLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity implements DialogInterface.OnClickListener {
 
     private static final int NOTIFICATION_REMINDER_NIGHT = 1;//this is for the broadcast receiver
     private Intent musicIntent;
-    private CardView popular1,popular2 ,popular3, movie01,movie02,movie04;
-    private RelativeLayout tvShowCard, popularCard, moviesCard, tvShows2Card,  romComsCard;
+   // private CardView popular1,popular2 ,popular3, movie01,movie02,movie04;
+   // private RelativeLayout tvShowCard, popularCard, moviesCard, tvShows2Card, romComsCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,17 +44,46 @@ public class MainActivity extends AppCompatActivity implements DialogInterface.O
         //hide bar
         getSupportActionBar().hide();
 
-        popularCard =findViewById(R.id.popular1);
-        popularCard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i= new Intent(MainActivity.this,DetailActivity.class);
-                i.putExtra("name","Divergent");
-                startActivity(i);
-            }
-        });
+        //fragments code
+
+        BottomNavigationView bottomNav =findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+       // popularCard =findViewById(R.id.popular1);
+       // popularCard.setOnClickListener(new View.OnClickListener() {
+         //   @Override
+            //public void onClick(View view) {
+            //    Intent i= new Intent(MainActivity.this,DetailActivity.class);
+              //  i.putExtra("name","Divergent");
+               // startActivity(i);
+          //  }
+      //  });
 
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment=null;
+
+            switch (item.getItemId()){
+                case R.id.nav_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.nav_favorite:
+                    selectedFragment = new FavoritesFragment();
+                    break;
+                case R.id.nav_search:
+                    selectedFragment = new SearchFragment();
+                    break;
+                case R.id.nav_person:
+                    selectedFragment = new ProfileFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+            return true;
+        }
+    };
 
     @Override
     public void onClick(DialogInterface dialogInterface, int i) {
@@ -72,6 +106,7 @@ public class MainActivity extends AppCompatActivity implements DialogInterface.O
         AlertDialog dialog = builder.create();
         dialog.show();
     }
+
 
 
 
